4fed974f2016838e91fee0aee06df89a382d030d
Exargna <fox7games@mail.ru>
2023-11-26 17:02:01 +0600

    Задание выполнено! С помощью цикла while.

--- Task 2/src/Main.java
+++ Task 2/src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         //todo Тут увольняем айтишников
+        int i = 0;
+        while (i < 100) {
+            i += 2;
+            System.out.println("Уволен айтишник с id" + i);
+            }
     }
 }
